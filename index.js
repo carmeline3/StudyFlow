ffbbf1417fcbba6eb6f71dddbc9483c67d8286ac
carmelinelibongi@gmail.com <you@example.com>
2026-05-26 01:16:27 +0200

    I made changes

--- index.js
+++ index.js
@@ -1,120 +1,60 @@
+
+let taskContainer = document.getElementById("taskContainer");
 let taskInput = document.getElementById("taskInput");
 let dueDateInput = document.getElementById("dueDate");
-let taskContainer = document.getElementById("taskContainer");
-
-let emailInput = document.getElementById("email");
 
 let taskArray = [];
 
-
-/* ADD TASK */
-
 function addTask(){
+    let taskName = taskInput.value;
+    let taskDate = dueDateInput.value;
+    if(taskName===""){
+        alert("Please fill in the task");
+         return;
+    }
+    else if(taskDate===""){
+         alert("Please fill in the due date");
+         return;
+    }
 
-let taskName = taskInput.value;
-let taskDate = dueDateInput.value;
-
-if(taskName === "" || taskDate === ""){
-alert("Please fill in all fields");
-return;
-}
-
-let taskObject = {
-id:Date.now(),
-name:taskName,
-date:taskDate,
-completed:false
-};
+    let taskObject = {
+        id: Date.now(),
+        name: taskName,
+        date: taskDate,
+        completed: false
+    };
 
 taskArray.push(taskObject);
 
-displayTasks();
+displayTask();
+taskInput.value="";
+dueDateInput.value="";
+};
 
-taskInput.value = "";
-dueDateInput.value = "";
 
+function displayTask(){
+    taskContainer.innerHTML = "";
+    taskArray.forEach(function(task){
+        taskContainer.innerHTML +=`
+        <div class="task">
+        <h2>${task.name}</h2>
+        <p>${task.date}</p>
+        </div>`;
+    });
 }
 
 
-/* COMPLETE TASK */
 
-function completeTask(id){
 
-taskArray = taskArray.map(function(task){
+let emailInput = document.getElementById("email");
 
-if(task.id === id){
-task.completed = !task.completed;
+function subscribe() {
+    if (emailInput.value === "") {
+        alert("Please enter your email");
+
+    }
+    else {
+        alert("Thank you for subcribing to StudyFlow");
+        emailInput.value = "";
+    }
 }
-
-return task;
-
-});
-
-displayTasks();
-
-}
-
-
-/* DELETE TASK */
-
-function deleteTask(id){
-
-taskArray = taskArray.filter(function(task){
-
-return task.id !== id;
-
-});
-
-displayTasks();
-
-}
-
-
-/* DISPLAY TASKS */
-
-function displayTasks(){
-
-taskContainer.innerHTML = "";
-
-taskArray.forEach(function(task){
-
-let taskDiv = document.createElement("div");
-
-taskDiv.classList.add("task");
-
-if(task.completed === true){
-
-taskDiv.style.opacity = "0.6";
-taskDiv.style.textDecoration = "line-through";
-
-}
-
-taskDiv.innerHTML = `
-
-<h3>${task.name}</h3>
-
-<p>${task.date}</p>
-
-<div class="task-buttons">
-
-<button onclick="completeTask(${task.id})">
-
-${task.completed ? "Undo" : "Complete"}
-
-</button>
-
-<button onclick="deleteTask(${task.id})">
-Delete
-</button>
-
-</div>
-
-`;
-
-taskContainer.appendChild(taskDiv);
-
-});
-
-}
-
-
